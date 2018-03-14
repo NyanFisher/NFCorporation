509a765e214854441e9245fee982ecf501a7d733
NyanFisher <zaikinmaxv@gmail.com>
2018-03-14 20:17:46 +0500

    Скрипт
    Вставил скрипт

--- NFCorporation/сайтецэ/script/script.js
+++ NFCorporation/сайтецэ/script/script.js
@@ -1,54 +1,72 @@
-var i = 0;
 var ck = true;
+//----------------------------------------------
+// Слздание нового блока Social, принимает блок в которые необходимо записать, блок прописывается в функции al в качестве параметра 'Div'. Перед этим в CSS сделать стиль этого блока
+//----------------------------------------------
+function createDiv(blockDiv) {
+    var newDiv = document.createElement('div');
+    newDiv.setAttribute('id', 'Social');
+    document.getElementById(blockDiv).appendChild(newDiv);
+    var hrefA = document.createElement('a')
+    hrefA.setAttribute('class', 'inputs');
+    hrefA.setAttribute('id', 'inpA');
+    hrefA.setAttribute('href', 'https://vk.com/NyanFisher');
+    hrefA.appendChild(document.createTextNode('Мой Вконтакте'));
 
-function al() {
+    document.getElementById('Social').appendChild(hrefA);
+    var hrefB = document.createElement('a');
+    hrefB.setAttribute('class', 'inputs');
+    hrefB.setAttribute('id', 'inpB');
+    hrefB.setAttribute('href', 'https://vk.com/id1');
+    hrefB.appendChild(document.createTextNode('Мой Инстаграм'));
+    document.getElementById('Social').appendChild(hrefB);
+}
+//----------------------------------------------
+// Удаление созданного блока
+//----------------------------------------------
+function removeDiv() {
+    document.getElementById('Social').remove();
+}
+//----------------------------------------------
+//Анимация картинки, функция принимает id картинки и блок(div) в который необходимо записать Анимацию появления нового блока.
+//----------------------------------------------
+
+function animateSocial(id,Div) {     
     if (ck) {
-        $('img').animate({
-            'left': '10',
-            'top': '200',
+        $('img#'+id).animate({    
+            'left': '12',
+            'top': '2630',
             'opacity': '1'
-        }, 1000);
-        clickDiv();
-        ck = false;
+        }, 500);
+        createDiv(Div);
+        animateDiv();
+        ck = false;   
     } else {
-        $('img').animate({
+        $('img#'+id).animate({
             'left': '30',
-            'top': '25',
+            'top': '2535',
             'opacity': '1'
-        }, 1000);
-        clickDiv();
-        document.getElementById('VkInst').remove();
+        }, 500);
+        animateDiv();
+        setTimeout(removeDiv,100);
         ck = true;
     }
 }
 
-function clickDiv() {
-    var newDiv = document.createElement('div');
-    newDiv.setAttribute('id', 'VkInst');
+//----------------------------------------------
+// Анимация всплывания блока
+//----------------------------------------------
 
-    if (i == 0 && ck) {
-        document.getElementById('head').appendChild(newDiv);
-        var hrefA = document.createElement('a')
-        hrefA.setAttribute('class', 'inputs');
-        hrefA.setAttribute('id', 'inpA');
-        hrefA.setAttribute('href', 'https://vk.com/NyanFisher');
-        hrefA.appendChild(document.createTextNode('Мой Вконтакте'));
-
-        document.getElementById('VkInst').appendChild(hrefA);
-        var hrefB = document.createElement('a');
-        hrefB.setAttribute('class', 'inputs');
-        hrefB.setAttribute('id', 'inpB');
-        hrefB.setAttribute('href', 'https://vk.com/id1');
-        hrefB.appendChild(document.createTextNode('Мой Инстаграм'));
-        document.getElementById('VkInst').appendChild(hrefB);
-        $('div').animate({
+function animateDiv() {
+    if (ck) {
+        $('div#Social').animate({
             'left': '100',
             'opacity': '1'
-        }, 1000);
-        i--;
+        }, 500);
     } else {
-
-        i++;
+        $('div#Social').animate({
+            'left': '-100',
+            'opacity': '1'
+        }, 500);
     }
 
 }
